0892945df21cd3b8ae4c8afc6eefe6a4715e7ab1
Bhavam <bhavambokare2008@outlook.com>
2021-09-20 06:32:21 +0530

    Explained the structure in C

--- demo.c
+++ demo.c
@@ -1,8 +1,9 @@
 // Explaing Basic Structure in C
-#include <stdio.h>
+#include <stdio.h> // Preprocessor Directive
 #include <conio.h>
-void main()
+void main() // Main Function
 {
-    printf("Hello C!!");
-    getch();
+    printf("Hello C!!"); // Display the line on the screen
+    getch();             /* to hold the output screen for some time until the user passes a key from 
+                the keyboard to exit the console screen */
 }
