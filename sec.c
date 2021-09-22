603dd052b59e72f48fdddba2972e552751cf1180
Bhavam <bhavambokare2008@outlook.com>
2021-09-22 15:28:04 +0530

    Printed 3 lines

---
+++ sec.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    printf("Hello World!! I am Learning C Programming");
+    printf("\nHello World!! I am Bhavam");
+    printf("\nHello Everyone!!");
+    return 0;
+    getch();
+}
