040c8803a83956b1415e597c704cb6637bd300e2
Bhavam <bhavambokare2008@outlook.com>
2021-09-23 15:31:31 +0530

    Program that displays output in the screen

---
+++ output.c
@@ -0,0 +1,11 @@
+// In this program, I am Displaying an output in the screen
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    printf("Hello World!!");
+    return 0;
+    getch();
+}
+// This program given me a Successful Output
