1fe530652156bb7a6f452157914d9efb56a2d493
Bhavam <bhavambokare2008@outlook.com>
2021-09-23 15:42:28 +0530

    Printing an output using Access Specifier

---
+++ out2.c
@@ -0,0 +1,11 @@
+//  In this Program, I printing output in the screen using access spciefier
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a = 10;
+    printf("The value in a is %d", a);
+    return 0;
+    getch();
+}
