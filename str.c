a7a0582beab99737324d230fee8055bef8d6be94
Bhavam <bhavambokare2008@outlook.com>
2021-09-24 15:13:10 +0530

    Asking user his/her Name

---
+++ str.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    char name[25];
+    printf("Enter your Name: ");
+    scanf("%s", &name);
+    printf("Your Name is %s", name);
+    return 0;
+}
