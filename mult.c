29a78cbd07db8339e589c985051882196ce75fba
Bhavam <bhavambokare2008@outlook.com>
2021-09-28 15:33:24 +0530

    Arithmetic Operators

---
+++ mult.c
@@ -0,0 +1,11 @@
+// Arithmetic Operators
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a;
+    a = 10 * 5;
+    printf("%d", a);
+    return 0;
+}
