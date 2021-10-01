f66c00f38cbedb4ff7c5cb3d23b666a10235c49d
Bhavam <bhavambokare2008@outlook.com>
2021-10-01 15:13:54 +0530

    Relational Operators

---
+++ rel.c
@@ -0,0 +1,11 @@
+// Relational Operators
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int y;
+    y = 5 > 4 > 3;
+    printf("%d", y);
+    return 0;
+}
