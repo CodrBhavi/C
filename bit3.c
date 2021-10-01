27bbe4e20287411b01a5a2e2b5f473a54035912d
Bhavam <bhavambokare2008@outlook.com>
2021-10-01 14:47:57 +0530

    Bitwise Operators

---
+++ bit3.c
@@ -0,0 +1,11 @@
+// Bitwise ^ Operators
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int y;
+    y = 23 ^ 56;
+    printf("%d", y);
+    return 0;
+}
---
+++ bit4.c
@@ -0,0 +1,11 @@
+// Bitwise >> Operator
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int z;
+    z = 23 >> 2;
+    printf("%d", z);
+    return 0;
+}
