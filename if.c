c8c98b079acb432e44fee1e49e5f2dbc20795767
Bhavam <bhavambokare2008@outlook.com>
2021-10-04 10:57:01 +0530

    If-else statement

---
+++ if.c
@@ -0,0 +1,19 @@
+// Decision Control Instructions
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int x;
+    printf("Enter a Number: ");
+    scanf("%d", &x);
+    if (3 <= 4)
+    {
+        printf("Hello World!!");
+    }
+    else
+    {
+        printf("Hello Code!!");
+    }
+    return 0;
+}
