53b34d243cd54fdff0ca61affb527c4fb8e4756d
Bhavam <bhavambokare2008@outlook.com>
2021-10-05 15:25:36 +0530

    While Loop Program

---
+++ while.c
@@ -0,0 +1,14 @@
+// While Loops
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int i = 1;     //Initialization
+    while (1 <= 1) //Condition
+    {
+        printf("Hello World!!");
+        i++; //Flow
+    }
+    return 0;
+}
