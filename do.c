ef0042e9e0587361b88c0a150cab74242a59219c
Bhavam <bhavambokare2008@outlook.com>
2021-10-06 15:19:05 +0530

    Do-while Loops

---
+++ do.c
@@ -0,0 +1,14 @@
+// Do-while loops
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    /* code */
+    int no = 1;
+    do
+    {
+        printf("\t %d", no);
+        no = no + 1;
+    } while (no <= 100);
+    return 0;
+}
