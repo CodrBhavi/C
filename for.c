626bf8c2668b8f21d4cdd92d407add3a4b4b6806
Bhavam <bhavambokare2008@outlook.com>
2021-10-08 11:32:45 +0530

    For Loop in C

---
+++ for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int no = 1;
+    for (no = 1; no <= 101; no = no + 2)
+    {
+        printf("\t%d", no);
+    }
+    return 0;
+}
